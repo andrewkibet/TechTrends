03875bb20867b1db3fcfe6cee5df215ef08cf8ba
andrewkibet <61360677+andrewkibet@users.noreply.github.com>
2022-07-08 04:36:10 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8r3mhg26jaug&)lie9pggs$pak+zv&_nl_5!*p$*5vn*wd2_!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
