4a0acc6f4c99408111a39b637d51587d24d1a2ea
Kibet <61360677+andrewkibet@users.noreply.github.com>
2025-06-13 15:22:41 +0300

    About log files

--- loganalysis.sh
+++ loganalysis.sh
@@ -1,1 +1,6 @@
-#!/bin/bash
+#!/bin/bashI 
+I have been reading about log analysis in linux systems. On Linux systems, 
+log files accumulate data from applications and system processes. They monitor 
+various conditions of application, state changes, information, and errors. Growing log files 
+can consume storage space and then be difficult to manage and review. Automated log rotation is
+ used to solve this storage issue.
