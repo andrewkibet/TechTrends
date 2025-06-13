7911b4e13957ff484949e82825ea4b928d01f09e
Kibet <61360677+andrewkibet@users.noreply.github.com>
2025-06-13 15:25:38 +0300

    Update loganalysis.sh

--- loganalysis.sh
+++ loganalysis.sh
@@ -5,3 +5,9 @@
 #various conditions of application, state changes, information, and errors. Growing log files 
 #can consume storage space and then be difficult to manage and review. Automated log rotation is
 # used to solve this storage issue.
+
+docker start splunk
+
+cd /etc/cron.daily #Logrotate is controlled by the cron system. Cron is a Linux service that allows you to run commands or scripts at 
+#pecific times or intervals—unattended. By default, your Linux system likely has a cron job set up to run logrotate daily. 
+#his cron job is usually located in a file /etc/cron.daily/logrotate.
